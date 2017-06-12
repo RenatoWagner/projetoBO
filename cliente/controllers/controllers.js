e936d4674e97d91ba9009fb5485d31009f38a6cb
Renato Wagner <renatowagnerx@gmail.com>
2017-06-12 00:49:24 -0300

    com gerarPDF :)

--- cliente/controllers/controllers.js
+++ cliente/controllers/controllers.js
@@ -14,6 +14,26 @@ $scope.mostrarBO = function(){
 	};
 
 
+  $scope.gerarPDF = function(){
+  var doc = new jsPDF();
+  doc.setFontSize(12);
+  doc.setFontStyle('bold');
+  doc.text('OCORRÊNCIA:', 20,50);
+  doc.text($scope.BO.ocorrencia, 20,60);
+  doc.text('DATA:', 20,70);
+  doc.text($scope.BO.data, 20,80);
+  doc.text('LOCAL:', 20,90);
+  doc.text($scope.BO.local, 20,100);
+  doc.text('ENVOLVIDOS:', 20,110);
+  doc.text($scope.BO.envolvidos, 20,120);
+  doc.text('BENS ENVOLVIDOS:', 20,130);
+  doc.text($scope.BO.bensEnvolvidos, 20,140);
+  doc.text('DINAMICA DO FATO:', 20,150);
+  doc.text($scope.BO.dinamicaDoFato, 20,160);
+  doc.save('arquivo.pdf');
+  }
+
+
   $scope.adicionarBO = function(){
 		$http.post('/api/BOs/', $scope.BO).then(function(response){
 			window.location.href = '/';
